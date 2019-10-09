59c10dc17a4b82db6f00d51adcc703c19b5151ae
Phil Sturgeon <me@philsturgeon.uk>
2019-10-09 19:40:53 +0200

    whole image is a link

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,34 +13,30 @@ export default ({ data }) => {
   const categories = data.allMarkdownRemark.edges.map(edge => edge.node.frontmatter);
   return (
     <>
-      <div className="padding">
-        <h1>Awesome Earth</h1>
+      <h1>Awesome Earth</h1>
 
-        <p>
-          A collection of resources, services, products, and ideas that you can
-          use to improve your impact on the environment.
-        </p>
-      </div>
+      <p>
+        A collection of resources, services, products, and ideas that you can
+        use to improve your impact on the environment.
+      </p>
 
       <Carousel />
 
-      <div className="padding">
-        <ul>
-          {categories.map(category => {
-            return (
-              <BackgroundImage
-                Tag="li"
-                className="category-image"
-                key={category.id}
-                fluid={category.image.childImageSharp.fluid}
-                backgroundColor={`#040e18`}
-              >
-                <Link to={category.path}>{category.name}</Link>
-              </BackgroundImage>
-            );
-          })}
-        </ul>
-      </div>
+      <ul>
+        {categories.map(category => {
+          return (
+            <BackgroundImage
+              Tag="li"
+              className="category-image"
+              key={category.id}
+              fluid={category.image.childImageSharp.fluid}
+              backgroundColor={`#040e18`}
+            >
+              <Link to={category.path}>{category.name}</Link>
+            </BackgroundImage>
+          );
+        })}
+      </ul>
     </>
   );
 };
