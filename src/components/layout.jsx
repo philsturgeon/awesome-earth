2c5b48154bf34c6a6509344b5ce849b025aca8a3
ElRaymond0 <phil.mayne1@gmail.com>
2019-11-03 20:57:00 +0000

    Tweak header component ordering

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -7,22 +7,20 @@ import SEO from "./seo";
 export default ({ title, seoTitle, image, children }) => {
   return (
     <>
-      <div>
-        <header>
-          <Fork />
-          <SEO
-            title={seoTitle}
-            description=""
-            keywords={[]}
-            meta={[]}
-            image={image}
-          />
-          <h1>
-            <a href="/">{title}</a>
-          </h1>
-        </header>
-        {children}
-      </div>
+      <SEO
+        title={seoTitle}
+        description=""
+        keywords={[]}
+        meta={[]}
+        image={image}
+      />
+      <header>
+        <Fork />
+        <h1>
+          <a href="/">{title}</a>
+        </h1>
+      </header>
+      {children}
     </>
   );
 };
