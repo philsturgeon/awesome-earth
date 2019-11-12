43bd0bb1053b33ec26aa02cd766cd33efec6dee0
Stephen Samra <stephen@stephensamra.com>
2019-11-12 22:31:44 +0000

    Distinguishing between countries that have content vs. ones that don't

--- src/pages/select-your-country.jsx
+++ src/pages/select-your-country.jsx
@@ -7,6 +7,12 @@ export default ({ data }) => {
     const countries = require('i18n-iso-countries').getNames('en');
     const [searchTerm, setSearchTerm] = useState('');
 
+    const countriesWithContent = [... new Set(data.allLinksYaml.edges
+      .map(({ node }) => node.countries)
+      .filter(countryList => !! countryList)
+      .flat(Infinity))]
+      .map(countryCode => countryCode.toUpperCase());
+
     return (
         <Layout title={data.site.siteMetadata.title} seoTitle="Select Your Country">
             <div className="select-your-country padding">
@@ -15,11 +21,32 @@ export default ({ data }) => {
                     <input type="text" placeholder="Filter Countries" onChange={e => setSearchTerm(e.target.value.toLowerCase())} />
                 </div>
                 <div>
-                    {Object.keys(countries).filter(countryCode => countries[countryCode].toLowerCase().includes(searchTerm)).map(countryCode => (
-                        <a className="country-list-item" href="#" key={countryCode}>
-                            {flag(countryCode)} {countries[countryCode]}
-                        </a>
-                    ))}
+                    {Object.keys(countries).filter(countryCode => countries[countryCode].toLowerCase().includes(searchTerm)).map(countryCode => {
+                        const countryHasContent = countriesWithContent.includes(countryCode)
+
+                        if (! countryHasContent) {
+                            return (
+                                <div className="country-list-item disabled" key={countryCode}>
+                                    {flag(countryCode)} {countries[countryCode]}
+                                    <div className="no-content-message">
+                                        <small>
+                                            We don't have any content for {countries[countryCode]}, yet. Why not {' '}
+                                            <a target="_blank" href="https://github.com/philsturgeon/awesome-earth/blob/master/CONTRIBUTING.md">
+                                                contribute something
+                                            </a>
+                                            ?
+                                        </small>
+                                    </div>
+                                </div>
+                            )
+                        }
+
+                        return (
+                            <div className="country-list-item" key={countryCode}>
+                                {flag(countryCode)} {countries[countryCode]} &rarr;
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         </Layout>
@@ -31,6 +58,13 @@ export const pageQuery = graphql`
     site {
       siteMetadata {
         title
+      }
+    }
+    allLinksYaml {
+      edges {
+        node {
+          countries
+        }
       }
     }
   }
