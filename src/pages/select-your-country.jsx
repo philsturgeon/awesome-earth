80e036ffc03cac4766c88860fcdfb3b33ee04c05
Stephen Samra <stephen@stephensamra.com>
2019-11-21 23:02:37 +0000

    Showing the currently selected country on the select your country page with an option to deselect it

--- src/pages/select-your-country.jsx
+++ src/pages/select-your-country.jsx
@@ -10,53 +10,68 @@ export default ({ data }) => {
     const [searchTerm, setSearchTerm] = useState('');
 
     const countriesWithContent = flattenDeep([...new Set(data.allLinksYaml.edges
-      .map(({ node }) => node.countries)
-      .filter(countryList => !! countryList))])
-      .map(countryCode => countryCode.toUpperCase());
+        .map(({ node }) => node.countries)
+        .filter(countryList => !!countryList))])
+        .map(countryCode => countryCode.toUpperCase());
 
     return (
         <Layout title={data.site.siteMetadata.title} seoTitle="Select Your Country">
-            <div className="select-your-country padding">
-                <div className="header">
-                    <h2>Select Your Country</h2>
-                    <input type="text" placeholder="Filter Countries" onChange={e => setSearchTerm(e.target.value.toLowerCase())} />
-                </div>
-                <div>
-                    <CountryContext.Consumer>
-                        {({ setCountry }) => Object.keys(countries).filter(countryCode => countries[countryCode].toLowerCase().includes(searchTerm)).map(countryCode => {
-                            const countryHasContent = countriesWithContent.includes(countryCode);
-                            const countryFlag = flag(countryCode);
-                            const countryName = countries[countryCode];
-
-                            if (! countryHasContent) {
-                                return (
-                                    <div className="country-list-item disabled" key={countryCode}>
-                                        {countryFlag} {countryName}
-                                        <div className="no-content-message">
-                                            <small>
-                                                We don't have any content for {countryName}, yet. Why not {' '}
-                                                <a target="_blank" href="https://github.com/philsturgeon/awesome-earth/blob/master/CONTRIBUTING.md" rel="noopener noreferrer">
-                                                    contribute something
-                                                </a>
-                                                ?
-                                            </small>
-                                        </div>
-                                    </div>
-                                );
-                            }
-
-                            return (
-                                <div className="country-list-item" key={countryCode} onClick={() => {
-                                    setCountry({ code: countryCode, name: countryName });
-                                    navigate("/");
-                                }}>
-                                    {countryFlag} {countryName} &rarr;
+            <CountryContext.Consumer>
+                {({ country, setCountry, clearCountry }) => (
+                    <div className="select-your-country padding">
+                        <div className="header">
+                            <h2>Select Your Country</h2>
+                            <input type="text" placeholder="Filter Countries" onChange={e => setSearchTerm(e.target.value.toLowerCase())} />
+                        </div>
+                        <div>
+                            {country.name !== null ?
+                                <div className="country-list-item selected" key={country.code}>
+                                    <small>Currently selected</small>
+                                    <div>{flag(country.code)} {country.name}</div>
+                                    <a href="#" onClick={e => {
+                                        e.preventDefault();
+                                        clearCountry();
+                                        navigate("/");
+                                    }}>Remove</a>
                                 </div>
-                            );
-                        })}
-                    </CountryContext.Consumer>
-                </div>
-            </div>
+                            : null}
+                            {Object.keys(countries).filter(countryCode => countries[countryCode].toLowerCase().includes(searchTerm)).map(countryCode => {
+                                const countryHasContent = countriesWithContent.includes(countryCode);
+                                const countryFlag = flag(countryCode);
+                                const countryName = countries[countryCode];
+
+                                if (! countryHasContent) {
+                                    return (
+                                        <div className="country-list-item disabled" key={countryCode}>
+                                            {countryFlag} {countryName}
+                                            <div className="no-content-message">
+                                                <small>
+                                                    We don't have any content for {countryName}, yet. Why not {' '}
+                                                    <a target="_blank" href="https://github.com/philsturgeon/awesome-earth/blob/master/CONTRIBUTING.md" rel="noopener noreferrer">
+                                                        contribute something
+                                                </a>
+                                                    ?
+                                            </small>
+                                            </div>
+                                        </div>
+                                    );
+                                }
+
+                                if (countryName !== country.name) {
+                                    return (
+                                        <div className="country-list-item" key={countryCode} onClick={() => {
+                                            setCountry({ code: countryCode, name: countryName });
+                                            navigate("/");
+                                        }}>
+                                            {countryFlag} {countryName} &rarr;
+                                    </div>
+                                    );
+                                }
+                            })}
+                        </div>
+                    </div>
+                )}
+            </CountryContext.Consumer>
         </Layout>
     );
 };
