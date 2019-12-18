a9d96208825f4f85d157c6e964ae37a3c00af248
Mike Bifulco <mbifulco@live.com>
2019-12-18 07:54:31 -0500

    pare back lots of CSS

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,7 @@ import React from "react";
 import { CountryProvider } from "./src/context/country-context";
 
 import "normalize.css";
+import "bootstrap/dist/css/bootstrap.css"
 import "./src/styles/global.scss";
 
 const wrapRootElement = ({ element }) => (
