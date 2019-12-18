b99bb9000c5fc167f98523e5cde4b44c789709f0
Mike Bifulco <mbifulco@live.com>
2019-12-18 16:52:44 -0500

    responsive grid rules

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -6,22 +6,20 @@ import Col from 'react-bootstrap/Col';
 
 const CategoryCard = ({ category }) => {
   return (
-    <Col xs={6} md={3}>
-      <Card style={{ width: '18rem' }}>
-        <img
-          className="card-image-top img-responsive"
-          style={{ maxHeight: '200px' }}
-          srcSet={category.image.childImageSharp.fluid.srcSet}
-          alt={category.title}
-        />
-        <Card.Body>
-          <Card.Title>{category.title}</Card.Title>
-          <Button className="mx-auto" variant="primary" href={category.slug}>
-            Act Now
-          </Button>
-        </Card.Body>
-      </Card>
-    </Col>
+    <Card>
+      <img
+        className="card-image-top img-fluid"
+        style={{ height: '200px', width: 'auto' }}
+        srcSet={category.image.childImageSharp.fluid.srcSet}
+        alt={category.title}
+      />
+      <Card.Body>
+        <Card.Title>{category.title}</Card.Title>
+        <Button className="mx-auto" variant="primary" href={category.slug}>
+          Act Now
+        </Button>
+      </Card.Body>
+    </Card>
   );
 };
 
