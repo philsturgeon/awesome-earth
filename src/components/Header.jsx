b4c972de59d22e4267ff98eba90f1df289423793
Mike Bifulco <mbifulco@live.com>
2019-12-18 11:33:18 -0500

    add search to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
 import Row from 'react-bootstrap/Row';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
@@ -23,12 +26,21 @@ const Header = ({ dark }) => (
             bg={dark ? 'dark' : 'light'}
             variant={dark ? 'dark' : 'light'}
           >
-            <Nav>
-              <Navbar.Brand>Awesom.earth</Navbar.Brand>
+            <Navbar.Brand>Awesom.earth</Navbar.Brand>
+            <Navbar.Collapse className="justify-content-end">
               <Nav.Link href="/">Home</Nav.Link>
               <Nav.Link href="/about">About</Nav.Link>
               <Nav.Link href="/contribute">Contribute</Nav.Link>
-            </Nav>
+
+              <Form inline>
+                <FormControl
+                  type="text"
+                  placeholder="Search"
+                  className="mr-sm-2"
+                />
+                <Button>Search</Button>
+              </Form>
+            </Navbar.Collapse>
           </Navbar>
         </Col>
       </Row>
