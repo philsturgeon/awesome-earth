13caf3f2ca2019e9f9dd0bd5f79afd23604d859a
Mike Bifulco <mbifulco@live.com>
2019-12-19 10:56:07 -0500

    add dropdown selector

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import Row from 'react-bootstrap/Row';
@@ -10,6 +12,33 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
 import { Fork } from '.';
+
+const useCases = [
+  {
+    tag: 'business',
+    title: 'Business Owner',
+  },
+  {
+    tag: 'homeowner',
+    title: 'Home Owner',
+  },
+  {
+    tag: 'nomad',
+    title: 'Nomad',
+  },
+  {
+    tag: 'commute',
+    title: 'Commuter',
+  },
+  {
+    tag: 'developers',
+    title: 'Developer',
+  },
+  {
+    tag: 'local-authorities',
+    title: 'Local Authority',
+  },
+];
 
 const Header = ({ dark }) => (
   <header
@@ -31,6 +60,13 @@ const Header = ({ dark }) => (
             <Navbar.Brand href="/">Awesom.earth</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse className="justify-content-end">
+              <DropdownButton title="I am a..." variant="Default">
+                {useCases.map(useCase => (
+                  <Dropdown.Item href={`tags/${useCase.tag}`}>
+                    {useCase.title}
+                  </Dropdown.Item>
+                ))}
+              </DropdownButton>
               <Nav.Link href="/about">About</Nav.Link>
               <Nav.Link href="/contribute">Contribute</Nav.Link>
 
